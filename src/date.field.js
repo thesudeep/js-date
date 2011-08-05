dcb69b02e5c9c1299a268fe13d251c26b8a37370
polischuk.victor <polischuk.victor@gmail.com>
2011-08-05 17:13:49 +0000

    test added. Year and Month fields are finished

---
+++ src/date.field.js
@@ -0,0 +1,85 @@
+Date.Field = {};
+// These are ints not enumerations as they represent genuine int values
+/** Constant (1) representing Monday, the first day of the week (ISO) */
+Date.Field.MONDAY = 1;
+/** Constant (2) representing Tuesday, the second day of the week (ISO) */
+Date.Field.TUESDAY = 2;
+/** Constant (3) representing Wednesday, the third day of the week (ISO) */
+Date.Field.WEDNESDAY = 3;
+/** Constant (4) representing Thursday, the fourth day of the week (ISO) */
+Date.Field.THURSDAY = 4;
+/** Constant (5) representing Friday, the fifth day of the week (ISO) */
+Date.Field.FRIDAY = 5;
+/** Constant (6) representing Saturday, the sixth day of the week (ISO) */
+Date.Field.SATURDAY = 6;
+/** Constant (7) representing Sunday, the seventh day of the week (ISO) */
+Date.Field.SUNDAY = 7;
+/** Constant (0) representing AM, the morning (from Calendar) */
+Date.Field.AM = 0;
+/** Constant (1) representing PM, the afternoon (from Calendar) */
+Date.Field.PM = 1;
+/** Constant (0) representing BC, years before zero (from Calendar) */
+Date.Field.BC = 0;
+/** Alternative constant (0) representing BCE, Before Common Era (secular) */
+Date.Field.BCE = 0;
+/**
+ * Constant (1) representing AD, years after zero (from Calendar).
+ * <p>
+ * All new chronologies with differrent Era values should try to assign
+ * eras as follows. The era that was in force at 1970-01-01 (ISO) is assigned
+ * the value 1. Earlier eras are assigned sequentially smaller numbers.
+ * Later eras are assigned sequentially greater numbers.
+ */
+Date.Field.AD = 1;
+/**
+ * Alternative constant (1) representing CE, Common Era (secular).
+ * <p>
+ * All new chronologies with differrent Era values should try to assign
+ * eras as follows. The era that was in force at 1970-01-01 (ISO) is assigned
+ * the value 1. Earlier eras are assigned sequentially smaller numbers.
+ * Later eras are assigned sequentially greater numbers.
+ */
+Date.Field.CE = 1;
+/** Milliseconds in one second (1000) (ISO) */
+Date.Field.MILLS_PER_SECOND = 1000;
+/** Seconds in one minute (60) (ISO) */
+Date.Field.SECONDS_PER_MINUTE = 60;
+/** Milliseconds in one minute (ISO) */
+Date.Field.MILLS_PER_MINUTE = Date.Field.MILLS_PER_SECOND * Date.Field.SECONDS_PER_MINUTE;
+/** Minutes in one hour (ISO) */
+Date.Field.MINUTES_PER_HOUR = 60;
+/** Seconds in one hour (ISO) */
+Date.Field.SECONDS_PER_HOUR = Date.Field.SECONDS_PER_MINUTE * Date.Field.MINUTES_PER_HOUR;
+/** Milliseconds in one hour (ISO) */
+Date.Field.MILLS_PER_HOUR = Date.Field.MILLS_PER_MINUTE * Date.Field.MINUTES_PER_HOUR;
+/** Hours in a typical day (24) (ISO). Due to time zone offset changes, the number of hours per day can vary. */
+Date.Field.HOURS_PER_DAY = 24;
+/** Minutes in a typical day (ISO). Due to time zone offset changes, the number of minutes per day can vary. */
+Date.Field.MINUTES_PER_DAY = Date.Field.MINUTES_PER_HOUR * Date.Field.HOURS_PER_DAY;
+/** Seconds in a typical day (ISO). Due to time zone offset changes, the number of seconds per day can vary. */
+Date.Field.SECONDS_PER_DAY = Date.Field.SECONDS_PER_HOUR * Date.Field.HOURS_PER_DAY;
+/** Milliseconds in a typical day (ISO). Due to time zone offset changes, the number of milliseconds per day can vary. */
+Date.Field.MILLS_PER_DAY = Date.Field.MILLS_PER_HOUR * Date.Field.HOURS_PER_DAY;
+/** Days in one week (7) (ISO) */
+Date.Field.DAYS_PER_WEEK = 7;
+/** Hours in a typical week. Due to time zone offset changes, the number of hours per week can vary. */
+Date.Field.HOURS_PER_WEEK = Date.Field.HOURS_PER_DAY * Date.Field.DAYS_PER_WEEK;
+/** Minutes in a typical week (ISO). Due to time zone offset changes, the number of minutes per week can vary. */
+Date.Field.MINUTES_PER_WEEK = Date.Field.MINUTES_PER_DAY * Date.Field.DAYS_PER_WEEK;
+/** Seconds in a typical week (ISO). Due to time zone offset changes, the number of seconds per week can vary. */
+Date.Field.SECONDS_PER_WEEK = Date.Field.SECONDS_PER_DAY * Date.Field.DAYS_PER_WEEK;
+/** Milliseconds in a typical week (ISO). Due to time zone offset changes, the number of milliseconds per week can vary. */
+Date.Field.MILLS_PER_WEEK = Date.Field.MILLS_PER_DAY * Date.Field.DAYS_PER_WEEK;
+
+Date.Field.assertTrue = function(condition, message) {
+    if (!condition) {
+        throw new Error(message);
+    }
+};
+Date.Field.validateInt = function(value) {
+    var i = parseInt(value, 10);
+
+    Date.Field.assertTrue(!isNaN(i), "Expected integer but was: " + value);
+
+    return i;
+};
---
+++ src/date.field.month.js
@@ -0,0 +1,99 @@
+Date.Field.Month = function(month) {
+    var MILLS_BY_NORMAL_MONTHS = [0, 2678400000,5097600000,7776000000,10368000000,13046400000,15638400000,18316800000,20995200000,23587200000,26265600000,28857600000];
+    var MILLS_BY_LEAP_MONTHS = [0, 2678400000,5184000000,7862400000,10454400000,13132800000,15724800000,18403200000,21081600000,23673600000,26352000000,28944000000];
+
+    var self = this;
+
+    this._year = new Date.Field.Year();
+
+    this.mills = function(value) {
+        if (arguments.length === 0) {
+            return self._year.mills() + (self._year.isLeap() ? MILLS_BY_LEAP_MONTHS[self._val] : MILLS_BY_NORMAL_MONTHS[self._val]);
+        }
+
+        value = Date.Field.validateInt(value);
+
+        self._year.mills(value);
+
+        // Copied from JodaTime.
+
+        var i = (value - self._year.mills()) / 1024;
+
+        // There are 86400000 milliseconds per day, but divided by 1024 is
+        // 84375. There are 84375 (128/125)seconds per day.
+
+        var month =
+            (self._year.isLeap())
+            ? ((i < 182 * 84375)
+               ? ((i < 91 * 84375)
+                  ? ((i < 31 * 84375) ? 1 : (i < 60 * 84375) ? 2 : 3)
+                  : ((i < 121 * 84375) ? 4 : (i < 152 * 84375) ? 5 : 6))
+               : ((i < 274 * 84375)
+                  ? ((i < 213 * 84375) ? 7 : (i < 244 * 84375) ? 8 : 9)
+                  : ((i < 305 * 84375) ? 10 : (i < 335 * 84375) ? 11 : 12)))
+            : ((i < 181 * 84375)
+               ? ((i < 90 * 84375)
+                  ? ((i < 31 * 84375) ? 1 : (i < 59 * 84375) ? 2 : 3)
+                  : ((i < 120 * 84375) ? 4 : (i < 151 * 84375) ? 5 : 6))
+               : ((i < 273 * 84375)
+                  ? ((i < 212 * 84375) ? 7 : (i < 243 * 84375) ? 8 : 9)
+                  : ((i < 304 * 84375) ? 10 : (i < 334 * 84375) ? 11 : 12)));
+
+        self._val = month - 1;
+
+        return self;
+    };
+
+    this.value = function(month) {
+        if (arguments.length === 0) {
+            return self._val + 1;
+        }
+
+        self._val = Date.Field.Month.validate(month) - 1;
+
+        return self;
+    };
+
+    if (arguments.length === 0) {
+        this.mills(new Date().getTime());
+    } else {
+        this.value(month);
+    }
+};
+
+/** Constant (1) representing January, the first month (ISO) */
+Date.Field.Month.JANUARY = 1;
+/** Constant (2) representing February, the second month (ISO) */
+Date.Field.Month.FEBRUARY = 2;
+/** Constant (3) representing March, the third month (ISO) */
+Date.Field.Month.MARCH = 3;
+/** Constant (4) representing April, the fourth month (ISO) */
+Date.Field.Month.APRIL = 4;
+/** Constant (5) representing May, the fifth month (ISO) */
+Date.Field.Month.MAY = 5;
+/** Constant (6) representing June, the sixth month (ISO) */
+Date.Field.Month.JUNE = 6;
+/** Constant (7) representing July, the seventh month (ISO) */
+Date.Field.Month.JULY = 7;
+/** Constant (8) representing August, the eighth month (ISO) */
+Date.Field.Month.AUGUST = 8;
+/** Constant (9) representing September, the nineth month (ISO) */
+Date.Field.Month.SEPTEMBER = 9;
+/** Constant (10) representing October, the tenth month (ISO) */
+Date.Field.Month.OCTOBER = 10;
+/** Constant (11) representing November, the eleventh month (ISO) */
+Date.Field.Month.NOVEMBER = 11;
+/** Constant (12) representing December, the twelfth month (ISO) */
+Date.Field.Month.DECEMBER = 12;
+
+Date.Field.Month.MIN_MONTH = Date.Field.Month.JANUARY;
+Date.Field.Month.MAX_MONTH = Date.Field.Month.DECEMBER;
+
+Date.Field.Month.validate = function(month) {
+    month = Date.Field.validateInt(month);
+
+    Date.Field.assertTrue(month >= Date.Field.Month.MIN_MONTH && month <= Date.Field.Month.MAX_MONTH,
+            "Month is expected to be in range [" + Date.Field.Month.MIN_MONTH + ".." + Date.Field.Month.MAX_MONTH + "] but was: " + month);
+
+    return month;
+};
