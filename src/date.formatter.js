5a6dbca7c449527a8d6089d9d4813589b9f264a1
polischuk.victor <polischuk.victor@gmail.com>
2011-09-21 17:21:18 +0000

    Date parse added.

--- src/date.formatter.js
+++ src/date.formatter.js
@@ -2,115 +2,249 @@ DateTime.Formatter = (function() {
     var Symbol = function(count, character) {
         this.format = function() {
             return DateTime.trail("", count, character);
-        }
+        };
+
+        this.parse = function(j, value, calendar) {
+            return j + count;
+        };
     };
 
     var Era = function(count) {
         this.format = function(calendar) {
             return calendar.withYear() < 0 ? "BC" : "AD";
-        }
+        };
+
+        this.parse = function(j, value, calendar) {
+            var val = value.substr(j, 2);
+
+            j += 2; //TODO<vpolischuk>: add era support in the calendar
+
+            switch (val) {
+                case "BC":
+                    calendar.withYear(-Math.abs(calendar.withYear()));
+                    break;
+                case "AD":
+                    calendar.withYear(Math.abs(calendar.withYear()));
+                    break;
+            }
+
+            return j;
+        };
     };
 
     var Year = function(count) {
         this.format = function(calendar) {
             return DateTime.trail(Math.abs(calendar.withYear()), count < 4 ? 2 : count);
-        }
+        };
+
+        this.parse = function(j, value, calendar) {
+            var val = DateTime.validateInt(value.substr(j, count));
+
+            if (count === 2) {
+                val += val < 32 ? 2000 : 1900;
+            }
+
+            calendar.withYear(val);
+
+            return j + count;
+        };
     };
 
     var Month = function(count) {
-        var fn;
+        var fnFormat;
+        var fnParse;
 
         switch (count) {
             case 1 :
-                fn = function(m) { return m };
+                fnFormat = function(m) { return String(m); };
+                fnParse = function(value) { return DateTime.validateInt(value)};
                 break;
             case 2 :
-                fn = function(m) { return DateTime.trail(m, 2) };
+                fnFormat = function(m) { return DateTime.trail(m, 2) };
+                fnParse = function(value) { return DateTime.validateInt(value)};
                 break;
             case 3 :
-                fn = function(m) { return "Short-" + m };
+                fnFormat = function(m) { return "Short-" + m };//TODO<vpolischu>: I18n settings required
+                fnParse = function(value) { return DateTime.validateInt(value)};
                 break;
             default:
-                fn = function(m) { return "Long-" + m };
+                fnFormat = function(m) { return "Long-" + m };//TODO<vpolischu>: I18n settings required
+                fnParse = function(value) { return DateTime.validateInt(value)};
         }
 
         this.format = function(calendar) {
-            return fn.call(this, calendar.withMonth());
-        }
+            return fnFormat.call(this, calendar.withMonth());
+        };
+
+        this.parse = function(j, value, calendar) {
+            var val = value.substr(j, count); //TODO<vpolischu>: I18n settings required.
+
+            calendar.withMonth(fnParse.call(this, val));
+
+            return j + count;
+        };
     };
 
     var WeekOfYear = function(count) {
         this.format = function(calendar) {
             return DateTime.trail(calendar.withWeekOfYear(), count);
-        }
+        };
+
+        this.parse = function(j, value, calendar) {
+            var val = DateTime.validateInt(value.substr(j, count));
+
+            calendar.withWeekOfYear(val);
+
+            return j + count;
+        };
     };
 
     var WeekOfMonth = function(count) {
         this.format = function(calendar) {
             return DateTime.trail(calendar.withWeekOfMonth(), count);
-        }
+        };
+
+        this.parse = function(j, value, calendar) {
+            var val = DateTime.validateInt(value.substr(j, count));
+
+            calendar.withWeekOfMonth(val);
+
+            return j + count;
+        };
     };
 
     var DateOfMonth = function(count) {
         this.format = function(calendar) {
             return DateTime.trail(calendar.withDayOfMonth(), count);
-        }
+        };
+
+        this.parse = function(j, value, calendar) {
+            var val = DateTime.validateInt(value.substr(j, count));
+
+            calendar.withDayOfMonth(val);
+
+            return j + count;
+        };
     };
 
     var DayNumeric = function(count) {
         this.format = function(calendar) {
             return DateTime.trail(calendar.withDayOfWeek(), count);
-        }
+        };
+
+        this.parse = function(j, value, calendar) {
+            var val = DateTime.validateInt(value.substr(j, count));
+
+            calendar.withDayOfWeek(val);
+
+            return j + count;
+        };
     };
 
     var DayText = function(count) {
         this.format = function(calendar) {
             return (count < 4 ? "Short-" : "Long-") + calendar.withDayOfWeek();
-        }
+        };
+
+        this.parse = function(j, value, calendar) { //TODO<vpolischu>: I18n settings required.
+            var val = DateTime.validateInt(value.substr(j, count));
+
+            calendar.withDayOfWeek(val);
+
+            return j + count;
+        };
     };
 
     var HourOfDay = function(count) {
         this.format = function(calendar) {
             return DateTime.trail(calendar.withHourOfDay(), count);
-        }
+        };
+
+        this.parse = function(j, value, calendar) {
+            var val = DateTime.validateInt(value.substr(j, count));
+
+            calendar.withHourOfDay(val);
+
+            return j + count;
+        };
     };
 
     var HourOfDayPlus = function(count) {
         this.format = function(calendar) {
             return DateTime.trail(calendar.withHourOfDay() + 1, count);
-        }
+        };
+
+        this.parse = function(j, value, calendar) {
+            var val = DateTime.validateInt(value.substr(j, count));
+
+            calendar.withHourOfDay(val);
+
+            return j + count;
+        };
     };
 
     var MinuteOfHour = function(count) {
         this.format = function(calendar) {
             return DateTime.trail(calendar.withMinuteOfHour(), count);
-        }
+        };
+
+        this.parse = function(j, value, calendar) {
+            var val = DateTime.validateInt(value.substr(j, count));
+
+            calendar.withMinuteOfHour(val);
+
+            return j + count;
+        };
     };
 
     var SecondOfMinute = function(count) {
         this.format = function(calendar) {
             return DateTime.trail(calendar.withSecondOfMinute(), count);
-        }
+        };
+
+        this.parse = function(j, value, calendar) {
+            var val = DateTime.validateInt(value.substr(j, count));
+
+            calendar.withSecondOfMinute(val);
+
+            return j + count;
+        };
     };
 
     var MillisOfSecond = function(count) {
         this.format = function(calendar) {
             return DateTime.trail(calendar.millis(), count);
-        }
+        };
+
+        this.parse = function(j, value, calendar) {
+            var val = DateTime.validateInt(value.substr(j, count));
+
+            calendar.withMillisOfSecond(val);
+
+            return j + count;
+        };
     };
 
     var TimeZoneText = function(count) {
         this.format = function(calendar) {
             return count < 4 ? calendar.withZone().id : calendar.withZone().name;
-        }
+        };
+
+        this.parse = function(j, value, calendar) {
+            throw new Error("not implemented yet"); //TODO<vpolischuk>: implement it
+        };
     };
 
-    var TimeZoneOffset = function(count) {
+    var TimeZoneOffset = function() {
         this.format = function(calendar) {
             var offset = DateTime.quotRem(DateTime.quotRem(calendar.withZone().offset(calendar.time()), DateTime.MILLIS_PER_MINUTE).quot, DateTime.MINUTES_PER_HOUR);
 
             return (offset.quot < 0 ? "-" : "+") + DateTime.trail(offset.quot, 2) + DateTime.trail(offset.rem, 2);
-        }
+        };
+
+        this.parse = function(j, value, calendar) {
+            throw new Error("not implemented yet"); //TODO<vpolischuk>: implement it
+        };
     };
 
     return function(pattern) {
@@ -171,6 +305,17 @@ DateTime.Formatter = (function() {
             }
 
             return text;
+        };
+
+        this.parse = function(value) {
+            var j = 0;
+            var calendar = new DateTime.Calendar(0, DateTime.TimeZone.DEFAULT);
+
+            for (var i in a) {
+                j = a[i].parse(j, value, calendar);
+            }
+
+            return calendar;
         };
     };
 })();
--- test/test.date.formatter.js
+++ test/test.date.formatter.js
@@ -5,62 +5,98 @@ DateTime.Formatter.Test.testCreation = function() {
     new DateTime.Formatter("GGGG yyyy MMMM wwww WWWW dddd FFFF EEEE HHHH kkkk mmmm ssss SSSS zzzz ZZZZ");
 };
 
-DateTime.Formatter.Test.test_G = function() {
+DateTime.Formatter.Test.testFormat_G = function() {
     var formatter = new DateTime.Formatter("G");
 
     assertEquals("AD", formatter.format(time(1970, 2, 3, 4, 5, 6, 231)));
 };
 
-DateTime.Formatter.Test.test_yyyy = function() {
+DateTime.Formatter.Test.testFormat_yyyy = function() {
     var formatter = new DateTime.Formatter("yyyy");
 
     assertEquals("1970", formatter.format(time(1970, 2, 3, 4, 5, 6, 231)));
 };
 
-DateTime.Formatter.Test.test_M = function() {
+DateTime.Formatter.Test.testFormat_M = function() {
     var formatter = new DateTime.Formatter("M");
 
     assertEquals("2", formatter.format(time(1970, 2, 3, 4, 5, 6, 231)));
 };
 
-DateTime.Formatter.Test.test_MM = function() {
+DateTime.Formatter.Test.testFormat_MM = function() {
     var formatter = new DateTime.Formatter("MM");
 
     assertEquals("02", formatter.format(time(1970, 2, 3, 4, 5, 6, 231)));
 };
 
-DateTime.Formatter.Test.test_w = function() {
+DateTime.Formatter.Test.testFormat_w = function() {
     var formatter = new DateTime.Formatter("w");
 
     assertEquals("6", formatter.format(time(1970, 2, 3, 4, 5, 6, 231)));
 };
 
-DateTime.Formatter.Test.test_ww = function() {
+DateTime.Formatter.Test.testFormat_ww = function() {
     var formatter = new DateTime.Formatter("ww");
 
     assertEquals("06", formatter.format(time(1970, 2, 3, 4, 5, 6, 231)));
 };
 
-DateTime.Formatter.Test.test_W = function() {
+DateTime.Formatter.Test.testFormat_W = function() {
     var formatter = new DateTime.Formatter("W");
 
     assertEquals("2", formatter.format(time(1970, 2, 3, 4, 5, 6, 231)));
 };
 
-DateTime.Formatter.Test.test_WW = function() {
+DateTime.Formatter.Test.testFormat_WW = function() {
     var formatter = new DateTime.Formatter("WW");
 
     assertEquals("02", formatter.format(time(1970, 2, 3, 4, 5, 6, 231)));
 };
 
-DateTime.Formatter.Test.test_d = function() {
+DateTime.Formatter.Test.testFormat_d = function() {
     var formatter = new DateTime.Formatter("d");
 
     assertEquals("3", formatter.format(time(1970, 2, 3, 4, 5, 6, 231)));
 };
 
-DateTime.Formatter.Test.test_dd = function() {
+DateTime.Formatter.Test.testFormat_dd = function() {
     var formatter = new DateTime.Formatter("dd");
 
     assertEquals("03", formatter.format(time(1970, 2, 3, 4, 5, 6, 231)));
+};
+
+DateTime.Formatter.Test.testParse_fullDate = function() {
+    var formatter = new DateTime.Formatter("yyyy-MM-dd");
+
+    assertEquals(time(2012, 11, 12), formatter.parse("2012-11-12").time());
+};
+
+DateTime.Formatter.Test.testParse_shortDate = function() {
+    var formatter = new DateTime.Formatter("yy-MM-dd");
+
+    assertEquals(time(1999, 11, 12), formatter.parse("99-11-12").time());
+};
+
+DateTime.Formatter.Test.testParse_shortDate19 = function() {
+    var formatter = new DateTime.Formatter("yy-MM-dd");
+
+    assertEquals(time(1932, 11, 12), formatter.parse("32-11-12").time());
+};
+
+DateTime.Formatter.Test.testParse_shortDate20 = function() {
+    var formatter = new DateTime.Formatter("yy-MM-dd");
+
+    assertEquals(time(2031, 11, 12), formatter.parse("31-11-12").time());
+};
+
+DateTime.Formatter.Test.testParse_withoutSeparators = function() {
+    var formatter = new DateTime.Formatter("yyyyMMdd");
+
+    assertEquals(time(2012, 11, 12), formatter.parse("20121112").time());
+};
+
+DateTime.Formatter.Test.testParse_fullDateTime = function() {
+    var formatter = new DateTime.Formatter("yyyy-MM-dd HH:mm");
+
+    assertEquals(time(2012, 11, 12, 1, 59), formatter.parse("2012-11-12 01:59").time());
 };
